2766f3a3dc7529329657a0cf7108ade1df5d0828
Josh Watts <josh@madebyanalogue.co.uk>
2026-01-21 19:32:22 +0000

    chore: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,6 +47,12 @@ export default defineNuxtConfig({
   
   app: {
     head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        }
+      ],
       link: [
         {
           rel: 'icon',
